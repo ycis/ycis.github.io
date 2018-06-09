d34c1d52603e1f4e90c98b6028a8e34119456076
Daniel Rudolph <w.daniel.rudolph@gmail.com>
2018-06-09 17:45:42 -0400

    testing single nav

---
+++ assets/javascript/code.js
@@ -0,0 +1,16 @@
+function returnLocString () {
+    var locArr, locStr = window.location.href;
+    locArr = locStr.split("/");
+    locArr = locArr[locArr.length - 1].split(".");
+    return locArr[locArr.length - 2]
+}
+
+$(document).ready(function () {
+    $(function(){
+        $("#nav-placeholder").load("nav.html");
+    });
+})
+var location;
+location = "#" + returnLocString();
+$(".nav-link").removeClass("active");
+$(location).addClass("active");
