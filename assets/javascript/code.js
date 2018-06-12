f56c6f426d7ef81fe1727b64682323ec8e585810
Daniel Rudolph <w.daniel.rudolph@gmail.com>
2018-06-12 15:55:06 -0400

    testing .load continues

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -5,10 +5,30 @@ function returnLocString () {
     return locArr[locArr.length - 2]
 }
 
+// function fillNavBar() {
+    
+// }
+
 $(document).ready(function () {
-    $("#nav-placeholder").load("nav.html");
+    var holderDiv = $("#nav-placeholder");
+    holderDiv.load("nav.html", function(responseTxt, statusTxt, xhr){
+        if(statusTxt == "success")
+            alert("External content loaded successfully!");
+        if(statusTxt == "error")
+            alert("Error: " + xhr.status + ": " + xhr.statusText);
+    });
+    var populatedNavDiv = $("#nav-placeholder");
+    console.log(populatedNavDiv.html());
+    // var navDiv = $("#nav-placeholder");
+    // var holderDiv = $("<div>").html('<object type="text/html" data="nav.html"></object>');
+    // console.log(holderDiv.html());
+    // document.getElementById("nav-placeholder").innerHTML='<object type="text/html" data="nav.html" ></object>';
+    // navDiv.append(holderDiv);
+    // var htmlStr = navDiv.text();
+    // console.log(htmlStr);
     var location;
     location = "#" + returnLocString();
     $(".nav-link").removeClass("active");
+    console.log(location);
     $(location).addClass("active");
 })
