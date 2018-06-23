744b84a61a5964bf695c6869835aa5b41e4d61bc
Daniel Rudolph <w.daniel.rudolph@gmail.com>
2018-06-22 23:50:34 -0400

    attempting to correct nav load issue

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,10 +1,18 @@
 
 
 $(document).ready(function () {
+    var cnt = 0;
     var navholder = $("#nav-placeholder");
+    var callinNameStr = navholder.attr("data-calling-page");
+    
     navholder.load("nav.txt")
     $(".nav-link").removeClass("active");
-    var jqEl = document.getElementById(navholder.attr("data-calling-page"));
+    do {
+        cnt++
+        var jqEl = document.getElementById(callinNameStr);
+    } while (jqEl != null || cnt > 100)
+
+    if (cnt > 100) {alert("failed to find elemant to update active")}
     jqEl.classList.add("active")
     jqEl.innerHTML = jqEl.innerHTML + '<span class="sr-only">(current)</span>';
 })
