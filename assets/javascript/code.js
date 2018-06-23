b0da1086c9dbdf0c5213c5266ee339f51b79cd28
Daniel Rudolph <w.daniel.rudolph@gmail.com>
2018-06-22 23:33:02 -0400

    update to include train activity

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,34 +1,10 @@
-function returnLocString () {
-    var locArr, locStr = window.location.href;
-    locArr = locStr.split("/");
-    locArr = locArr[locArr.length - 1].split(".");
-    return locArr[locArr.length - 2]
-}
 
-// function fillNavBar() {
-    
-// }
 
 $(document).ready(function () {
-    var holderDiv = $("#nav-placeholder");
-    holderDiv.load("nav.html", function(responseTxt, statusTxt, xhr){
-        if(statusTxt == "success")
-            alert("External content loaded successfully!");
-        if(statusTxt == "error")
-            alert("Error: " + xhr.status + ": " + xhr.statusText);
-    });
-    var populatedNavDiv = $("#nav-placeholder");
-    console.log(populatedNavDiv.html());
-    // var navDiv = $("#nav-placeholder");
-    // var holderDiv = $("<div>").html('<object type="text/html" data="nav.html"></object>');
-    // console.log(holderDiv.html());
-    // document.getElementById("nav-placeholder").innerHTML='<object type="text/html" data="nav.html" ></object>';
-    // navDiv.append(holderDiv);
-    // var htmlStr = navDiv.text();
-    // console.log(htmlStr);
-    var location;
-    location = "#" + returnLocString();
+    var navholder = $("#nav-placeholder");
+    navholder.load("nav.txt")
     $(".nav-link").removeClass("active");
-    console.log(location);
-    $(location).addClass("active");
+    var jqEl = document.getElementById(navholder.attr("data-calling-page"));
+    jqEl.classList.add("active")
+    jqEl.innerHTML = jqEl.innerHTML + '<span class="sr-only">(current)</span>';
 })
